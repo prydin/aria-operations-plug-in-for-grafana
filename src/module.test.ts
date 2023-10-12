9a63b845d3260ee942abb60014a81e23d0268f8a
prydin <prydin@vmware.com>
2023-10-12 15:20:39 -0400

    WIP

--- src/module.test.ts
+++ src/module.test.ts
@@ -42,8 +42,6 @@ import {
   SlidingStdDev,
   SlidingSum,
   SortedBag,
-  estimateBandwidth,
-  lambertW,
 } from 'smoother';
 import { AggregationSpec, CompiledQuery, SlidingWindowSpec } from 'types';
 
@@ -635,13 +633,7 @@ describe('Sliding functions', () => {
     }
   });
 
-  test('Gaussian math', () => {
-    expect(lambertW(1)).toBeCloseTo(0.56714, 4);
-    expect(lambertW(1000)).toBeCloseTo(5.2496, 4);
-    expect(lambertW(0.5)).toBeCloseTo(0.351734, 4);
-    expect(lambertW(1 / 144)).toBeCloseTo(0.006897, 4);
-    expect(estimateBandwidth(1 / 12)).toBeCloseTo(0.090058, 4);
-  });
+  test('Gaussian math', () => {});
 
   test('Gaussian', () => {
     const g = new GaussianEstimator(300000, 8640000, { duration: 60000 });
