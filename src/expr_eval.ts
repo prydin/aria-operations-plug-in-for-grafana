1ff66637b98dbdb84affc8fc1e52a5e32b48ac89
prydin <prydin@vmware.com>
2024-06-18 15:52:15 -0400

    First compiling version of expression eval

--- src/expr_eval.ts
+++ src/expr_eval.ts
@@ -1,109 +1,121 @@
-import { DataFrame, Field, Vector } from "@grafana/data";
-import { time } from "console";
-import { DataFrameBundle, ExpressionEvaluator, ValueBundle } from "types";
-
+import {
+  DataFrame,
+  Field,
+  FieldType,
+  Labels,
+  MutableDataFrame,
+  Vector,
+} from '@grafana/data';
+import { ExpressionEvaluator } from 'types';
 
 interface ValueStore {
+  [key: string]: {
     [key: string]: {
-        [key: string]: {
-            timestamps: Vector<number>
-            values: Vector<number>
-        }
-    }
+      timestamps: Vector<number>;
+      values: Vector<number>;
+      labels?: Labels;
+    };
+  };
 }
 
-export const evaulateExpression = (expr: ExpressionEvaluator, data: DataFrame[]): DataFrame[] => {
-    // Collect list of timestamps. We can pick a metric at random, since a specific 
-    // timestamp needs to be present in all series in order to produce a valid result.
-    let timestamps: Vector<number> | undefined;
-    const frame = data[0];
-    const tsField = frame.fields.find((f) => f.name === "Time");
-    timestamps = tsField?.values;
-    if(!timestamps) {
-        // No timestamps => empty result. Not much to do.
-        console.log("Warning: Timestamps missing for series. Skipping expression")
-        return [];
+export const evaulateExpression = (
+  expr: ExpressionEvaluator,
+  data: DataFrame[],
+  resultRefId: string,
+  resultName: string
+): DataFrame[] => {
+  // Collect list of timestamps. We can pick a metric at random, since a specific
+  // timestamp needs to be present in all series in order to produce a valid result.
+  const frame = data[0];
+  const tsField = frame.fields.find((f) => f.name === 'Time');
+  const timestamps = tsField?.values;
+  if (!timestamps) {
+    // No timestamps => empty result. Not much to do.
+    console.log('Warning: Timestamps missing for series. Skipping expression');
+    return [];
+  }
+
+  // Stuff the frame data into a friendlier (and faster) structure
+  const metricInstances: { [key: string]: Labels | undefined } = {};
+  const valueStore: ValueStore = {};
+  for (const frame of data) {
+    if (!frame.refId) {
+      continue;
     }
-
-    // Stuff the frame data into a friendlier (and faster) structure
-    const metricInstances: { [key: string]: boolean} = {}
-    const valueStore: ValueStore = {};
-    for(const frame of data) {
-        if(!frame.refId) {
-            continue;
-        }
-        if(!valueStore[frame.refId]) {
-            valueStore[frame.refId] = {};
-        }
-        for(const field of frame.fields) {
-            let values = null;
-            let timestamps = null;
-            if(field.name === "Time") {
-                timestamps = field.values;
-            }
-            if(field.name === "Value") {
-                values = field.values;
-            }
-            let labelKey = ""
-            for(const key in field.labels) {
-                labelKey += key + ":" + field.labels[key] + ","
-            }
-            if(!(timestamps && values)) {
-                break;
-            }
-            const frameKey = frame.name + labelKey;
-            valueStore[frame.refId][frameKey] = { timestamps, values};
-            metricInstances[frameKey] = true;
-        }
+    if (!valueStore[frame.refId]) {
+      valueStore[frame.refId] = {};
     }
-
-    // Evaluate expression for each timestamp
-    for(var i = 0; i < timestamps.length; ++i) {
-        for(const frameKey in metricInstances) {                                                                          
-            const getter = (key: string): number => {
-                const entry = valueStore[key][frameKey];
-                if(!(entry.timestamps && entry.values)) {
-                    return NaN;
-                }
-                const idx = findTSIndex(entry.timestamps, timestamps!.get(i));
-                return entry.values.get(idx);
-            }
-        } 
+    for (const field of frame.fields) {
+      let values = null;
+      let timestamps = null;
+      if (field.name === 'Time') {
+        timestamps = field.values;
+      }
+      if (field.name === 'Value') {
+        values = field.values;
+      }
+      let labelKey = '';
+      for (const key in field.labels) {
+        labelKey += key + ':' + field.labels[key] + ',';
+      }
+      if (!(timestamps && values)) {
+        break;
+      }
+      const frameKey = frame.name + labelKey;
+      const labels = field.labels;
+      valueStore[frame.refId][frameKey] = { timestamps, values, labels };
+      metricInstances[frameKey] = labels;
     }
+  }
 
+  const result: DataFrame[] = [];
+  for (const frameKey in metricInstances) {
+    const labels = metricInstances[frameKey];
+    const frame = new MutableDataFrame({
+      refId: resultRefId,
+      name: resultName,
+      fields: [
+        { name: 'Time', type: FieldType.time },
+        { name: 'Value', type: FieldType.number, labels },
+      ],
+    });
 
-    // For each variable...
-    for(const refId in data) {
-        // Get the list of frames for this variable. There will be one frame per metric,
-        // but each metric can be subdivided by tags.
-        const frameList = data[refId]
-        for(const frame of frameList) {
+    for (var i = 0; i < timestamps.length; ++i) {
+      const timestamp = timestamps.get(i);
 
-
-            let key = frame.name + "("
-            for(const field of frame.fields) {
-                key += field.name + "(";
-                for()
-                key += ","
-                
-            }
+      // Define the getter function
+      const getter = (key: string): number => {
+        const entry = valueStore[key][frameKey];
+        if (!(entry.timestamps && entry.values)) {
+          return NaN;
         }
+        const idx = findTSIndex(entry.timestamps, timestamp);
+        return entry.values.get(idx);
+      };
+      const value = expr(getter);
+      if (isNaN(value)) {
+        continue;
+      }
+      frame.add({ Time: timestamp, Value: value });
     }
-}
+    result.push(frame);
+  }
+  return result;
+};
 
 const findTSIndex = (timestamps: Vector<number>, ts: number): number => {
-    let step = timestamps.length / 2;
-    let pos = step;
-    while(step > 0) {
-        if(timestamps.get(pos) === ts) {
-            return pos;
-        }
-        if(timestamps.get(pos) > ts) {
-            pos -= step;
-        } else {
-            pos += step;
-        }
+  let step = timestamps.length / 2;
+  let pos = step;
+  while (step > 0) {
+    if (timestamps.get(pos) === ts) {
+      return pos;
     }
-    // Couldn't find it.
-    return NaN
-}
+    if (timestamps.get(pos) > ts) {
+      pos -= step;
+    } else {
+      pos += step;
+    }
+  }
+  // Couldn't find it.
+  return NaN;
+};
